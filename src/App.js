fedac469717f0b273f8e7971b0523dbf643928a4
Aditya <holaaditya123@gmail.com>
2023-10-16 18:30:15 +0530

    Basic CSS done commiting

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import {
   WhatGPT3,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
